8d040a91e59f43873466509230af5861fd8bb4bf
Hui Zhang <hvzhang@gmail.com>
2018-07-06 14:48:18 +0800

    add two files

---
+++ src/main/java/cn/ah/myword/DemoApplication.java
@@ -0,0 +1,18 @@
+package cn.ah.myword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoApplication.class, args);
+
+
+
+
+
+    }
+}
